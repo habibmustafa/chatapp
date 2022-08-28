c33ee14f9730746fa71ab8214a4c618c133d75f4
habibmustafa <hebibullahmustafazade@gmail.com>
2022-08-28 11:33:37 +0400

    deploy

--- server/index.js
+++ server/index.js
@@ -5,14 +5,28 @@ const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
 const msgRoutes = require("./routes/msgRoutes");
 const app = express();
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+   res.header(
+      "Access-Control-Allow-Headers, *, Access-Control-Allow-Origin",
+      "Origin, X-Requested-with, Content_Type,Accept,Authorization",
+      "http://localhost:3000"
+   );
+   if (req.method === "OPTIONS") {
+      res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET");
+      return res.status(200).json({});
+   }
+   next();
+});
 
-const dbURL =
-   "mongodb+srv://habibmustafa:hebib24589@chat-app.uibf8p5.mongodb.net/?retryWrites=true&w=majority";
 mongoose
-   .connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+   .connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+   })
    .then(() => console.log("Succesfull"))
    .catch((err) => console.log(err.message));
 
@@ -20,7 +34,7 @@ app.use("/auth", authRoutes);
 app.use("/msg", msgRoutes);
 
 // listening
-const server = app.listen("5000", () => {
+const server = app.listen(process.env.PORT || 5000, () => {
    console.log("Listening...");
 });
 
@@ -46,32 +60,3 @@ io.on("connection", (socket) => {
       }
    });
 });
-
-// let clients = [];
-
-// socket.io connection
-// io.on("connection", (socket) => {
-//    console.log("a user connected:", socket.id);
-
-//    socket.on("storeClientInfo", (data) => {
-//       let clientInfo = new Object();
-
-//       clientInfo.customId = data.customId;
-//       clientInfo.clientId = socket.id;
-//       clients.push(clientInfo);
-//    });
-
-//    socket.on("disconnect", (data) => {
-//       for (let i = 0, len = clients.length; i < len; i++) {
-//          let c = clients[i];
-//          if (c.clientId == socket.id) {
-//             clients.splice(i, 1);
-//             break;
-//          }
-//       }
-//    });
-
-// socket.on("chat", (data) => {
-//    io.sockets.emit("chat", data);
-// });
-// });
